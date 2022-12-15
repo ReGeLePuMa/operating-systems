b9efd7002ba72c92aba3edb190bb11c1830b8da7
ReGeLePuMa <scpetrea@gmail.com>
2022-12-15 11:51:50 +0200

    Terminat Lab IO part 3

---
+++ content/chapters/io/lab/support/multiplex/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM_CLIENTS=8
+
+for i in $(seq 1 $NUM_CLIENTS); do
+    cat msg.txt | grep "$i" > temp.txt
+    wait
+    nc -N localhost 42424 < temp.txt
+done
+rm temp.txt
+wait
+
